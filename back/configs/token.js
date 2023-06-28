a56586544d0f15210cd4025f63649025aaba286f
FranMack <franmack556@gmail.com>
2023-06-28 13:40:03 -0300

    Login routes

--- back/configs/token.js
+++ back/configs/token.js
@@ -1,1 +1,14 @@
-console.log("token.js");
+const jwt=require ("jsonwebtoken");
+const SECRET= "proyect3d";
+
+const generateToken = (payload) => {
+    const token = jwt.sign({ user: payload }, SECRET, { expiresIn: "2h" });
+  
+    return token;
+  };
+
+  function validateToken(token) {
+    return jwt.verify(token, SECRET);
+  }
+  
+  module.exports = { generateToken, validateToken };
