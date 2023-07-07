241cc6f58bd372acd0899c91c9495c7ab2a49471
cantarodev <jhon.cantaro@unas.edu.pe>
2023-07-07 13:53:46 +0100

    Create environment variables finish

--- back/configs/db.js
+++ back/configs/db.js
@@ -1,8 +1,13 @@
 const Sequelize = require("sequelize");
-const db = new Sequelize("project_ecommerce", null, null, {
-  host: "localhost",
-  dialect: "postgres",
-  logging: false,
-});
+const db = new Sequelize(
+  "project_ecommerce",
+  process.env.USER_DB,
+  process.env.PASS_DB,
+  {
+    host: process.env.HOST,
+    dialect: "postgres",
+    logging: false,
+  }
+);
 
 module.exports = db;
