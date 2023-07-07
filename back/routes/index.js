93becf750f3451be844db42b9c70886c5c184904
gerpag <germinalfp@gmail.com>
2023-07-07 14:10:30 -0300

    update branch

--- back/routes/index.js
+++ back/routes/index.js
@@ -6,9 +6,10 @@ const adminRoute = require("./admin.route");
 const { getProductSearch } = require("../controllers/search.controller");
 
 const orderRouter = require("./order.route");
+
 const reviewRoute = require("./review.route");
-const shopingRoute=require("./shopping.route")
-const emailRoute=require("./email.route")
+const shopingRoute = require("./shopping.route");
+const emailRoute = require("./email.route");
 
 const router = express.Router();
 
@@ -18,10 +19,11 @@ router.use("/category", categoryRoute);
 router.get("/search", getProductSearch);
 router.use("/admin", adminRoute);
 
+router.use("/shopping", shopingRoute);
 router.use("/order", orderRouter);
-router.use("/review", reviewRoute);
-router.use("/shopping",shopingRoute);
-router.use("/send-email",emailRoute)
 
->
+router.use("/review", reviewRoute);
+router.use("/shopping", shopingRoute);
+router.use("/send-email", emailRoute);
+
 module.exports = router;
